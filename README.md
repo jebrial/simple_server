9e4c24b0c11c4844917e9288adc665d8e594daa3
Gabriel Dalay <gdalay@jibe.com>
2016-01-17 16:44:54 -0500

    Updated simple server to be actually simple

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Simple Server: for static single page AngularJS development
+#Simple Server: for static single page AngularJS development
 ===========================================================
 
-by Gabe Dalay
+###by Gabe Dalay
 ----------------------
 
 ** To run:
@@ -11,6 +11,9 @@ Just grab the executable and place it where you please.
 
 Then at the commandline from the directory you put it:
 
-./simple_server -directory=YOUR_ABSOLUTE_DIRECTORY_HERE
+   `./simple_server <DIR>
+
+* Or just run:
+	`go run main.go <DIR>
 
 *Not for production unless you want ants... because that's how you get ants.
--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
 	"net/http"
-	"time"
+	"os"
 )
 
 func main() {
-	directoryPtr := flag.String("directory", "public", "directory string to file server")
-	flag.Parse()
-	fmt.Println("..simple server started at: ", time.Now())
-	fmt.Printf("..serving the directory: '%s' on port 8080", *directoryPtr)
-	err := http.ListenAndServe(":8080", http.FileServer(http.Dir(*directoryPtr)))
-	if err != nil {
-		log.Printf("Error running web server for static assets: %v", err)
+	if len(os.Args) == 1 {
+		panic("No folder specified")
 	}
+	folder := os.Args[1]
+	http.Handle("/", http.FileServer(http.Dir(folder)))
+	http.ListenAndServe(":8080", nil)
 }
